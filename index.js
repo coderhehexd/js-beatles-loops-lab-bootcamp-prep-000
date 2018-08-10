0b47583f513346f8178c6c1ef5f12cfbc5b6fb20
Chris Li <cli78822@gmail.com>
2018-08-10 05:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(let i = 0; i < musicians.length; i++){
+    arr[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return arr;
+}
+function johnLennonFacts(arr){
+ for(let i = 0; i < arr.length; i++){
+   arr[i]+= "!!!";
+ } 
+ return arr
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  var x = num;
+  do{
+    arr.push("I love the Beatles!")
+    x++;
+  }while(num > 15)
+}
